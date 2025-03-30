ae0b60e0a3eeb7c6ab1a85284196dca5aac4aedd
CAP882 <160057433+ChrisPaladino@users.noreply.github.com>
2025-03-30 18:50:45 -0400

    Update main.py

--- _archive/src/main.py
+++ _archive/src/main.py
@@ -13,7 +13,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
 from src.game_logic import Game
-from src.game_gui import GameGUI
+from gui.game_gui import GameGUI
 from src.game_utilities import logger
 
 def main():
